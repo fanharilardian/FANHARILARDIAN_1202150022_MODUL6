18c1ea7717fd86b2ff6c48ea13422500adb9892f
fanharilardian <fanharilfanharil@gmail.com>
2018-04-01 14:47:40 +0700

    Commit Pertama

---
+++ app/src/main/java/modul6/praktikum/fanharil/fanharilardian_1202150022_modul6/MainActivity.java
@@ -0,0 +1,13 @@
+package modul6.praktikum.fanharil.fanharilardian_1202150022_modul6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
